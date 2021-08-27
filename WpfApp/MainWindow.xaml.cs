d15666efe5fadefc2e4b53833fb368cb5d4eea7e
Hampus Aronsson <hampzz99@gmail.com>
2021-08-27 13:20:52 +0200

    test

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CTRLC_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hej");
+        }
+
+        private void CTRLV_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CTRLZ_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CTRLY_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CTRLX_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
